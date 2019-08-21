96bd1293f01d465e6d6e68feae368777dd820a04
leofusco89 <leofusco89@gmail.com>
2019-08-21 14:25:56 -0300

    Final commit

--- app/src/main/java/com/example/toolbarapp/MainActivity.java
+++ app/src/main/java/com/example/toolbarapp/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.toolbarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Mis libros");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_ejemplo, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_op1:
+                //Realizar acción
+                break;
+            case R.id.item_op2:
+                //Realizar acción
+                break;
+            default:
+                //Realizar acción
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
